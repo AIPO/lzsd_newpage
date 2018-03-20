d982b25503384e5b9fad3093d2848408f480b61f
Povilas Vaitonis <povilas.vaitonis@mediapark.com>
2018-03-20 10:04:06 +0200

    Added PagesController

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function getIndex()
+    {
+        
+    }
+
+    public function getAbout()
+    {
+        
+    }
+
+    public function getContact()
+    {
+        
+    }
+
+    public function postContact()
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/about', 'PagesController@getAbout');
+Route::get('/contacts', 'PagesController@getContact');
+Route::get('/', 'PagesController@getIndex');
